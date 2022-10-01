395d7c428e80fee10901ce50d3c356f1a99758c0
Kevin <kraeme1k@hs-pforzheim.de>
2022-10-01 14:36:41 +0200

    Added examples for usage of cpp- and python-interface

---
+++ example/cpp-interface/test_vecx_rl.cpp
@@ -0,0 +1,65 @@
+/**********************************************************************************************************
+ * Simple example of the usage of the cpp interface with random inputs instead of a machine learning agent
+ **********************************************************************************************************/
+
+#include "environment.hpp"
+
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv)
+{
+    std::pair<int, int> image_dims = {40 /* widht */, 60 /* height */};
+    std::vector<uint8_t> pixels;
+
+    vecx_rl::environment env = vecx_rl::environment(1, true, true, true, image_dims);
+
+    std::string cartfilename = std::string("frog_jump.bin");
+
+    try
+    {
+        env.load_rom(cartfilename);
+    }
+    catch (vecx_rl::unsupported_rom& e)
+    {
+        std::cerr << e.what() << '\n';
+        std::exit(-1);
+    }
+    catch (std::filesystem::filesystem_error& e)
+    {
+        std::cerr << e.what() << '\n';
+        std::exit(-1);
+    }
+
+    vecx_rl::action a;
+    vecx_rl::reward_t r = 0;
+
+    std::srand(std::time(nullptr)); // use current time as seed for random generator
+    uint16_t rand_action = 0;
+
+    std::vector<uint8_t> legal_actions = env.get_legal_actions();
+
+    for (size_t episode = 0; episode < 10; ++episode)
+    {
+        while (!env.is_game_finished())
+        {
+            rand_action = std::rand();
+            a.set_action(
+                legal_actions.at(
+                    rand_action % legal_actions.size()));
+            r += env.step(a);
+
+            pixels = env.get_image().value_or(std::vector<uint8_t>{});
+
+            // feed reward and image to a reinforcement learning agent ...
+        }
+
+        std::cout << "Episode: " << episode << ", Score: " << r << "\n";
+        env.reset();
+        r = 0;
+    }
+
+    return 0;
+}
---
+++ example/python-interface/test_pyvecx_rl.py
@@ -0,0 +1,48 @@
+#************************************************************************************************************
+# Simple example of the usage of the python interface with random inputs instead of a machine learning agent
+#************************************************************************************************************
+
+from random import randint
+
+import sys
+sys.path.append(r'../build/python/')
+
+import pyvecx_rl
+
+if __name__ == "__main__":
+    image_dims : tuple = (40, 60)
+    pixels : list = []
+
+    env = pyvecx_rl.environment(frames_per_step = 25,
+        real_time = True, 
+        enable_window = True,
+        enable_sound = True,
+        image_dims = image_dims
+    )
+    
+    cartridge = "frog_jump.bin"
+
+    try:
+        env.load_rom(cartridge)
+    except RuntimeError as re:
+        print(re)
+        exit(-1)
+
+    action = pyvecx_rl.action()
+    reward : int = 0
+
+    legal_actions : list = env.get_legal_actions()
+
+    for episode in range(0, 10):
+        while(not env.is_game_finished()):
+            action.event = legal_actions[randint(0,9) % len(legal_actions)]
+
+            reward += env.step(action)
+
+            pixels = env.get_image()
+
+            # feed reward and image to a reinforcement learning agent ...
+
+        print("Episode: {}, Score: {}".format(episode, reward))
+        env.reset()
+        reward = 0
