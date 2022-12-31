6c1ef324a5d700df357451d75a7d7615692756af
Illegalfan <kaiserma@hs-pforzheim.de>
2022-12-31 15:18:24 +0100

    Adjusted paths in testModel.py

--- train/testModel.py
+++ train/testModel.py
@@ -8,11 +8,10 @@ env.reset()
 
 #set path to trained model here
 
-
-model_path = "/home/illegalfan/Programming/Vectrex/repo2/Vectrex_RL/train/models/20_11_2022_12_52_28/2140000.zip"
+model_path = "models/PPO/2140000.zip"
 model = PPO.load(model_path, env=env)
 
-#model_path = "/home/illegalfan/Programming/Vectrex/repo2/Vectrex_RL/train/models/18_11_2022_12_50_29/2680000.zip"
+#model_path = "models/DQN/2680000.zip"
 #model = DQN.load(model_path, env=env)
 
 for ep in range(500):
